f1a861a63b8f4bfcb423ff40eb8f6829308582c0
Ali Abbas <ali@grappetite.com>
2019-05-31 11:28:02 +0500

    ios code restructured

--- ios/Classes/SwiftWcFlutterSharePlugin.swift
+++ ios/Classes/SwiftWcFlutterSharePlugin.swift
@@ -10,41 +10,40 @@ public class SwiftWcFlutterSharePlugin: NSObject, FlutterPlugin {
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
-        case "text":
-            onText(args: call.arguments)
-        case "file":
-            onFile(args: call.arguments)
+        case "share":
+            onShare(args: call.arguments)
+            break;
         default:
             result(FlutterMethodNotImplemented)
         }
     }
-    
-    public func onText(args: Any?) {
-        let argsMap = args as! NSDictionary
-        let text:String = argsMap.value(forKey: "text") as! String
-        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [text], applicationActivities: nil)
+    public func onShare(args: Any?) {
         
-        // present the view controller
-        let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
-        activityViewController.popoverPresentationController?.sourceView = controller.view
-        
-        controller.show(activityViewController, sender: self)
-    }
-    public func onFile(args: Any?) {
         let argsMap = args as! NSDictionary
-        let fileName:String = argsMap.value(forKey: "fileName") as! String
-        let textToShare:String? = argsMap.value(forKey: "textToShare") as? String
-        // load the file
-        let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!;
-        let contentUri = NSURL(fileURLWithPath: docsPath).appendingPathComponent(fileName)
-        var contentToShare:[Any] = [];
-        contentToShare.append(contentUri!);
-        if (textToShare != nil) {
-            contentToShare.append(textToShare!)
+        
+        let text:String? = argsMap.value(forKey: "text") as? String
+        let subject:String? = argsMap.value(forKey: "subject") as? String
+        let fileName:String? = argsMap.value(forKey: "fileName") as? String
+        
+        var contentToShare:[Any] = []
+        
+        if(fileName != nil) {
+            // load the file
+            let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!
+            let contentUri = NSURL(fileURLWithPath: docsPath).appendingPathComponent(fileName!)
+            contentToShare.append(contentUri!)
+        }
+        
+        if (text != nil) {
+            contentToShare.append(text!)
         }
         
         // set up activity view controller
         let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: contentToShare, applicationActivities: nil)
+        if(subject != nil) {
+            contentToShare.append(subject!)
+            activityViewController.setValue(subject!, forKey: "Subject")
+        }
         
         // present the view controller
         let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
