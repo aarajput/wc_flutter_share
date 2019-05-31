826c3cdad202387bab2b40462f0b229219c3a221
Ali Abbas <ali@grappetite.com>
2019-05-31 10:50:08 +0500

    android structure changed

--- android/src/main/kotlin/com/wisecrab/wc_flutter_share/WcFlutterSharePlugin.kt
+++ android/src/main/kotlin/com/wisecrab/wc_flutter_share/WcFlutterSharePlugin.kt
@@ -1,6 +1,7 @@
 package com.wisecrab.wc_flutter_share
 
 import android.content.Intent
+import androidx.annotation.Nullable
 import androidx.core.content.FileProvider
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -23,48 +24,38 @@ class WcFlutterSharePlugin(private val registrar: Registrar) : MethodCallHandler
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
-            "text" -> onText(call.arguments)
-            "file" -> onFile(call.arguments)
+            "share" -> onShare(call.arguments)
             else -> result.notImplemented()
         }
     }
 
-    @Suppress("UNCHECKED_CAST")
-    private fun onText(args: Any) {
-        val argMap = args as Map<String, String>
-        val title = argMap["title"]
-        val textToSend = argMap["text"]
-        val mimeType = argMap["mimeType"]
-
-        val context = registrar.activeContext()
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.type = mimeType
-        shareIntent.putExtra(Intent.EXTRA_TEXT, textToSend)
-        context.startActivity(Intent.createChooser(shareIntent, title))
-
-    }
-
 
     @Suppress("UNCHECKED_CAST")
-    private fun onFile(arguments: Any) {
+    private fun onShare(arguments: Any) {
         val argMap = arguments as Map<String, String>
+
         val sharePopupTitle = argMap["sharePopupTitle"]
-        val textToShare = argMap["textToShare"]
-        val subjectToShare = argMap["subjectToShare"]
+        val text = argMap["text"]
+        val subject = argMap["subject"]
         val fileName = argMap["fileName"]
-        val mimeTypeOfFile = argMap["mimeTypeOfFile"]
+        val mimeType = argMap["mimeType"]
         val activeContext = registrar.activeContext()
 
         val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.type = mimeTypeOfFile
-        val file = File(activeContext.cacheDir, fileName)
-        val fileProviderAuthority = activeContext.packageName + PROVIDER_AUTH_EXT
-        val contentUri = FileProvider.getUriForFile(activeContext, fileProviderAuthority, file)
-        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
-        if (textToShare != null)
-            shareIntent.putExtra(Intent.EXTRA_TEXT, textToShare)
-        if (subjectToShare != null)
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subjectToShare)
+        shareIntent.type = mimeType
+
+        if (text != null)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, text)
+        if (subject != null)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+
+        if (fileName != null) {
+            val file = File(activeContext.cacheDir, fileName)
+            val fileProviderAuthority = activeContext.packageName + PROVIDER_AUTH_EXT
+            val contentUri = FileProvider.getUriForFile(activeContext, fileProviderAuthority, file)
+            shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
+        }
+
         activeContext.startActivity(Intent.createChooser(shareIntent, sharePopupTitle))
     }
 }
