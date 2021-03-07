f681f203e594551028cdd2d8d12f4e407c9a981d
Ali Abbas <16102514+aarajput@users.noreply.github.com>
2021-03-07 10:44:38 +0500

    Android: android sharing restructuring (fixed)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,84 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 import 'package:wc_flutter_share/wc_flutter_share.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await WcFlutterShare.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('How to: wc_flutter_share'),
+          textTheme: TextTheme(button: TextStyle(color: Colors.black)),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              onPressed: _shareText,
+              child: Text('Share text'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              onPressed: _shareImage,
+              child: Text('Share file only'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              onPressed: _shareImageAndText,
+              child: Text('Share file and text'),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _shareText() async {
+    try {
+      WcFlutterShare.share(
+          sharePopupTitle: 'Share',
+          subject: 'This is subject',
+          text: 'This is text',
+          mimeType: 'text/plain');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _shareImage() async {
+    try {
+      final ByteData bytes = await rootBundle.load('assets/wisecrab.png');
+      await WcFlutterShare.share(
+          sharePopupTitle: 'share',
+          fileName: 'share.png',
+          mimeType: 'image/png',
+          bytesOfFile: bytes.buffer.asUint8List());
+    } catch (e) {
+      print('error: $e');
+    }
+  }
+
+  void _shareImageAndText() async {
+    try {
+      final ByteData bytes = await rootBundle.load('assets/wisecrab.png');
+      await WcFlutterShare.share(
+          sharePopupTitle: 'share',
+          subject: 'This is subject',
+          text: 'This is text',
+          fileName: 'share.png',
+          mimeType: 'image/png',
+          bytesOfFile: bytes.buffer.asUint8List());
+    } catch (e) {
+      print('error: $e');
+    }
   }
 }
